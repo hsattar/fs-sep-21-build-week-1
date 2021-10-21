79be60f467126dda0f164a4ea1adf5b28e201064
Francesco Micunco <80624232+FrancescoMicunco@users.noreply.github.com>
2021-10-22 01:08:27 +0200

    refactorin

--- genres/genres.js
+++ genres/genres.js
@@ -10,52 +10,6 @@ let rebSection = document.getElementById("rebStyle")
 let jazzSection = document.getElementById("jazzStyle")
 let modernSection = document.getElementById("modernStyle")
 
-
-
-
-
-
-
-
-const songs = [{
-        id: "r01",
-        genre: "Rock",
-        artist: "Pink Floyd",
-        albumTitle: "Dark Side of the Moon",
-        song: "Money"
-    },
-    {
-        id: "r02",
-        genre: "Rock",
-        artist: "Pink Floyd",
-        albumTitle: "Dark Side of the Moon",
-        song: "On the Run"
-    },
-    {
-        id: "r03",
-        genre: "Rock",
-        artist: "Pink Floyd",
-        albumTitle: "Dark Side of the Moon",
-        song: "Time"
-    },
-    {
-        id: "r04",
-        genre: "Rock",
-        artist: "Pink Floyd",
-        albumTitle: "Dark Side of the Moon",
-        song: "Eclipse"
-    },
-    {
-        id: "r05",
-        genre: "Rock",
-        artist: "Pink Floyd",
-        albumTitle: "Dark Side of the Moon",
-        song: "The great Gig in the Sky"
-    },
-
-
-
-];
 const arrFavSong = []
 
 btnRock.addEventListener("click", function() {
@@ -79,68 +33,55 @@ btnModern.addEventListener("click", function() {
 })
 
 // == select song as favourite
-//let heartIcons = document.querySelector("i")
-//let classActive = icondoorstHeart.classList.contains("bi-heart")
 
 
 let heartIcons = document.querySelectorAll("i")
-let classActive = heartIcons[0].classList.contains("bi-heart")
 
-//.classList.contains("bi-heart")
 
-let addFavouritTotheList = function() {
+heartIcons.addEventListener("click", function(e) {
+        heartIcons.forEach(element => {
+                if (heartIcons.classList.contains("bi-heart")) { //if the preferred heart is false
+                    heartIcons.classList.remove("bi-heart"); //  we change class of heart
+                    heartIcons.classList.add("bi-heart-fill") // ==========================
 
-    if (classActive) { //if the preferred heart is false
-        heartIcons.classList.remove("bi-heart"); //  we change class of heart
-        heartIcons.classList.add("bi-heart-fill") // ==========================
-            //let favSong = this.previousElementSibling; // here we select the favourite song and his content
-            //let selectedElement = favSong.innerHTML; //==============================================
-            //heartIcons.push(selectedElement) // here we add the son at the array of favourite song reay to be show
-        console.log(arrFavSong) // ===========================================
-    } else if (!classActive) {
-        heartIcons.classList.remove("bi-heart-fill"); //if the preferred heart is true
-        heartIcons.classList.add("bi-heart") //  we change class of heart
-            //let favSong = this.previousElementSibling; // here we select the favourite song and his content
-            //let selectedElement = favSong.innerHTML; //==============================================
-            //let index = arrFavSong.indexOf(selectedElement) // here we check if the song is into the array
-            // if (index >= 0) { // if it's true
-            //   arrFavSong.splice(index) // we delete the song
-    }
+
+
+
+                } else if (!heartIcons.classList.contains("bi-heart")) {
+                    heartIcons.classList.remove("bi-heart-fill"); //if the preferred heart is true
+                    heartIcons.classList.add("bi-heart") //  we change class of heart
+                    let favSong = this.previousElementSibling; // here we select the favourite song and his content
+                    let selectedElement = favSong.innerHTML; //==============================================
+                    let index = arrFavSong.indexOf(selectedElement) // here we check if the song is into the array
+                    if (index >= 0) { // if it's true
+                        arrFavSong.splice(index) // we delete the song
+
+                    }
+
+
+                });
+
+
+
+
+        }) // end function add event listener
+
+
+
+
+    let favSong = this.previousElementSibling; // here we select the favourite song and his content
+    let selectedElement = favSong.innerHTML; //==============================================
+    heartIcons.push(selectedElement) // here we add the son at the array of favourite song reay to be show
+    console.log(arrFavSong) // ===========================================
+}
+else if (!classActive) {
+
+}
 }
 
 
 
-heartIcons.addEventListener("click", addFavouritTotheList)
 
-
-
-
-
-
-
-
-/* let addFavouritTotheList = function() {
-
-    let classActive = heartIcons.classList.contains("bi-heart")
-    if (classActive) { //if the preferred heart is false
-        heartIcons.classList.remove("bi-heart"); //  we change class of heart
-        heartIcons.classList.add("bi-heart-fill") // ==========================
-        let favSong = this.previousElementSibling; // here we select the favourite song and his content
-        let selectedElement = favSong.innerHTML; //==============================================
-        arrFavSong.push(selectedElement) // here we add the son at the array of favourite song reay to be show
-        console.log(arrFavSong) // ===========================================
-    } else if (!classActive) {
-        heartIcons.classList.remove("bi-heart-fill"); //if the preferred heart is true
-        heartIcons.classList.add("bi-heart") //  we change class of heart
-        let favSong = this.previousElementSibling; // here we select the favourite song and his content
-        let selectedElement = favSong.innerHTML; //==============================================
-        let index = arrFavSong.indexOf(selectedElement) // here we check if the song is into the array
-        if (index >= 0) { // if it's true
-            arrFavSong.splice(index) // we delete the song
-        }
-        console.log(arrFavSong)
-    }
-} */
 
 
 
